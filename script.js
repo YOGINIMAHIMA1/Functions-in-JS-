5416ae3b2bf2436eb826923c03d92e463e185046
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2023-11-14 19:14:27 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function add(a,b)
+{
+  return a+b;
+}
+console.log(add(23,8));
+console.log(add(10,20));
+var ans= add(12,3);
+console.log(ans);
+function sayHello()
+{
+  console.log('Hello everyone');
+}
+console.log(sayHello());
